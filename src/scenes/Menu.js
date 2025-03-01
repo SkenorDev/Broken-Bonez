294a50d47eefd86d94a0ecb83c5386af43ebccca
Nathan Skinner <naskinne@ucsc.edu>
2025-03-01 13:34:08 -0800

    added collision

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,38 +1,45 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-      super("menuScene")
-    }
-    preload() {
-        // load images/tile sprites
-         this.load.path = './assets/'
-         this.load.image('tilesetImage','tileset.png')
-         this.load.tilemapTiledJSON('tilemapJSON','BB_Start.json')
-         this.load.image('bike','motorbike.png')
-      }
-    create() {
-      const map=this.add.tilemap('tilemapJSON')
-      const tileset = map.addTilesetImage('tileset','tilesetImage')
+  constructor() {
+      super("menuScene");
+  }
+
+  preload() {
+      this.load.path = './assets/';
+      this.load.image('tilesetImage', 'tileset.png');
+      this.load.tilemapTiledJSON('tilemapJSON', 'BB_Start.json');
+      this.load.image('bike', 'motorbike.png');
+  }
+
+  create() {
+      const map = this.add.tilemap('tilemapJSON');
+      const tileset = map.addTilesetImage('tileset', 'tilesetImage');
+
+      const skyLayer = map.createLayer('sky', tileset, 0, 0);
+      const decLayer = map.createLayer('decorations', tileset, 0, 0);
+      const groundLayer = map.createLayer('ground', tileset, 0, 0);
+
+      // Enable collision on ground layer
+      groundLayer.setCollisionByProperty({ collides: true });
+      groundLayer.setCollisionByExclusion([-1]);
+      console.log(groundLayer.layer.collideIndexes);
+      // Create bike with physics
+      this.bike = this.physics.add.sprite(100, 100, 'bike');
+      this.bike.setGravityY(150);
+      this.bike.setCollideWorldBounds(true);
+
+      // Add collider between bike and ground
+      this.physics.add.collider(this.bike, groundLayer);
+
+      // Camera settings
+      this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+      this.cameras.main.startFollow(this.bike, true, 0.25, 0.25);
+
+      // Debugging (Uncomment to see collision boxes)
+       this.physics.world.createDebugGraphic();
+       groundLayer.renderDebug(this.add.graphics(), { tileColor: null, collidingTileColor: new Phaser.Display.Color(255, 0, 0, 100) });
+  }
+
+  update() {
      
-      
-      const skyLayer =map.createLayer('sky',tileset,0,0)
-      const decLayer =map.createLayer('decorations',tileset,0,0)
-      const groundLayer =map.createLayer('ground',tileset,0,0)
-      this.bike = this.physics.add.sprite(100, 100, 'bike', 0)
-      groundLayer.setCollisionByProperty({collides:true})
-      //this.physics.world.setBounds(0,0,map.widthInPixels,map.heightInPixels)
-      this.cameras.main.setBounds(0,0,map.widthInPixels,map.heightInPixels)
-      this.cameras.main.startFollow(this.bike,true,0.25,0.25)
-//groundLayer.setCollisionByProperty({collides:true})
-this.physics.add.collider(this.bike,groundLayer)
-this.bike.setGravityY(100)
-this.bike.body.setCollideWorldBounds(true)
-
-      }
-
-
-      update() {
-         
-      }
-      
-    }
-  
+  }
+}
