ed59685a45b7b0650389ae93ea6a5e70aa3ff221
SkenorDev <naskinne@ucsc.edu>
2025-03-10 15:33:06 -0700

    Reverted to added logic version

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -82,11 +82,10 @@ class Play extends Phaser.Scene {
 if(!this.cursors.left.isDown) {
   this.bike.setDragX(40)
 }
-if(this.cursors.up.isDown && !this.bike.body.blocked.down) {
-    console.log("eaten")
+if(this.cursors.up.isDown && !this.bike.body.blocked.down&&this.uponce<1) {
   this.bike.setAngularVelocity(360);
   this.mult=this.mult+1
-  //this.uponce++
+  this.uponce++
 }
 
 
